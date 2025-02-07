c5c8e072bb6c0fa43c7c5c3992a8fe6dfc39996a
kirill <kozhemyachenkokirill05@gmail.com>
2025-02-07 13:29:14 +0300

    Initial commit

---
+++ my-react-app/src/App.js
@@ -0,0 +1,14 @@
+
+
+import './styles/App.css';
+import RegistForm from './components/UI/RegistForm';
+
+function App() {
+  return (
+    <div className="App">
+      <RegistForm></RegistForm>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-react-app/src/components/UI/RegistForm.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+
+function RegistForm() {
+   const [name, setName] = useState('');
+   const [password, setPassword] = useState('');
+
+   return (
+      <form>
+         <input type="text" placeholder="Имя" value={name} onChange={e => setName(e.target.value)}/>
+         <input type="text" placeholder="Пароль" value={password} onChange={e => setPassword(e.target.value)}/>
+         <button>Зарегистрироваться</button>
+         {/* <p>или войдите через Google</p>
+         <div><img src="./assets/images/google-icon.svg" alt="google"></img></div> */}
+      </form>
+   )
+}
+
+export default RegistForm;
